8739ae7d2c8a5e0b870092cb991e6e52f4bc6f7f
BiswadeepDas-sp <82663470+BiswadeepDas-sp@users.noreply.github.com>
2024-10-18 21:37:53 +0530

    Update kinematics_solver.py

--- kinematics_solver.py
+++ kinematics_solver.py
@@ -1,148 +1,130 @@
-def input_params():
+params=("i_vel","f_vel","acc","dis","time")
+
+def user_input():
     param_values=[]
     for param in params:
-        try:
-            param_value=float(input(f"enter {param} = "))
-        except:
-            param_value="unknown"
-        param_values.append(param_value)
+       try:
+          param_values.append(float(input(f"enter {param} ")))
+       except ValueError:
+          param_values.append("unknown")
     return param_values
 
+param_values=user_input()
+
 def solvable():
-    if param_values.count("unknown")>2:
-     not_solvable=True
-     return not_solvable
-    
-def to_find():
-     to_find=[]
-     for i,param_value in enumerate(param_values):
-         if param_value=="unknown":
-            to_find.append(params[i])
-     return to_find
+  if param_values.count("unknown")>2:
+     solvable=False
+  else:
+     solvable=True
+  return solvable
 
+solvable=solvable()
+def sort():
+   to_find=[]
+   given=[]
+   for i,param_value in enumerate(param_values):
+      if param_value=="unknown":
+         to_find.append(params[i])
+      else:
+         given.append(params[i])
+   return to_find,given
+to_find,given=sort()
 
-def solve_i_vel():
-    try:
-        var=f_vel-acc*time
-    except:
-        try:
-            var=((f_vel)**2-2*acc*dis)**0.5
-        except:
-            try:
-                var=(dis-0.5*acc*time**2)/time
-            except:
-                var=(2*dis-f_vel*time)/time
+i_vel,f_vel,acc,dis,time=param_values[0],param_values[1],param_values[2],param_values[3],param_values[4]
 
+def solve_i_vel(given):
+    if "f_vel" and "acc" and "time" in given:
+         var=f_vel-acc*time
+    elif "f_vel" and "acc" and "dis" in given:
+         var=((f_vel)**2-2*acc*dis)**0.5
+    elif "acc" and "dis" and "time" in given:
+         var=(dis-0.5*acc*time**2)/time
+    elif "f_vel" and "dis" and "time" in given:
+         var=(2*dis-f_vel*time)/time 
+    else:
+        raise ValueError("cannot solve")
     return var
 
-def solve_f_vel():
-    try:
-        var=i_vel+acc*time
-    except:
-        try:
-            var=((i_vel)**2+2*acc*dis)**0.5
-        except:
-            var=(2*dis-f_vel*time)/time
-    
+def solve_f_vel(given):
+    if "i_vel" and "acc" and "time"in given:
+         var=i_vel+acc*time
+    elif "i_vel" and "acc" and "dis" in given:
+         var=((i_vel)**2+2*acc*dis)**0.5
+    elif "i_vel"and "dis" and "time" in given:
+         var=(2*dis-i_vel*time)/time
+    elif "acc" and "dis" and "time" in given:
+         var=(dis+0.5*acc*time**2)/time
+    else:
+        raise ValueError("cannot solve")
     return var
 
-def solve_acc():
-    try:
-        var=(f_vel-i_vel)/time
-    except:
-        try:
-            var=(f_vel**2-i_vel**2)/(2*dis)
-        except:
-            var=2*(dis-i_vel*time)/time**2
+def solve_acc(given):
+    if "i_vel" and "f_vel" and "time" in given:
+         var=(f_vel-i_vel)/time
+    elif "i_vel" and "dis" and "time" in given:
+         var=2*(dis-i_vel*time)/time**2
+    elif "i_vel" and "f_vel" and "dis" in given:
+         var=(f_vel**2-i_vel**2)/(2*dis)
+    elif "f_vel" and "dis" and "time" in given:
+         var=2*(f_vel*time-dis)/time**2
+    else:
+        raise ValueError("cannot solve")
     return var
 
-def solve_dis():
-    try:
-        var=(f_vel**2-i_vel**2)/(2*acc)
-    except:
-        try:
-            var=i_vel*time+0.5*acc*time**2
-        except:
-            var=(i_vel+f_vel)/2*time
+def solve_dis(given):
+    if "i_vel" and "acc" and "time" in given:
+         var=i_vel*time+0.5*acc*time**2
+    elif "i_vel" and "acc" and "f_vel" in given:
+         var=((f_vel)**2-(i_vel)**2)/(2*acc)
+    elif "i_vel" and "f_vel" and "time" in given:
+         var=(i_vel+f_vel)/2*time
+    elif "acc" and "f_vel" and "time" in given:
+         var=f_vel*time-0.5*acc*time**2
+    else:
+        raise ValueError("cannot solve")
     return var
 
-def solve_time():
-    try:
-        var=(f_vel-i_vel)/acc
-    except:
-        try: 
-            var =((-i_vel)+((i_vel**2+2*acc*dis))**0.5)/acc
-        except:
-            var=2*dis/(i_vel+f_vel)
+def solve_time(given):
+    if "i_vel" and "acc" and "f_vel" in given:
+         var=(f_vel-i_vel)/acc
+    elif "i_vel" and "acc" and "f_vel" in given:
+         var=(-i_vel+(i_vel**2-2*acc*dis))/acc
+    elif "i_vel" and "f_vel" and "dis" in given:
+         var=2*dis/(i_vel+f_vel)
+    elif "acc" and "f_vel" and "dis" in given:
+         var=(f_vel-(f_vel**2-2*acc*dis))/acc
+    else:
+        raise ValueError("cannot solve")
     return var
+
+functions=[solve_i_vel,solve_f_vel,solve_acc,solve_dis,solve_time]
+operations=dict(zip(params,functions))
+
 
 def solve(param_values):
-    if "i_vel" in to_find:
-        try:
-             i_vel=solve_i_vel()
-
-        except:
-            i_vel=str("cannot solve")
-    else:
-        i_vel=param_values[0]
-
-    if "f_vel" in to_find:
-        try:
-             f_vel=solve_f_vel()
-
-        except:
-            f_vel=str("cannot solve")
-    else:
-        f_vel=param_values[1]
-    if "acc" in to_find:
-        try:
-             acc=solve_acc()
-
-        except:
-            acc=str("cannot solve")
-    else:
-        acc=param_values[2]
-    if "dis" in to_find:
-        try:
-             dis=solve_dis()
-
-        except:
-            dis=str("cannot solve")
-    else:
-        dis=param_values[3]
-    if "time" in to_find:
-        try:
-            time= solve_time()
-
-        except:
-            time=str("cannot solve")
-    else:
-        time=param_values[4]
-    solved_param_values=[i_vel,f_vel,acc,dis,time]
+    solved_param_values=[]
+    for i,param in enumerate(params):
+        if param in to_find:
+            try:
+                solved_param_value=operations[param](given)
+                solved_param_values.append(solved_param_value)
+            except ValueError:
+                solved_param_values.append("unknown")
+        else:
+            solved_param_values.append(param_values[params.index(param)])
     return solved_param_values
-            
+
+         
+
 def main():
-    if not_solvable:
-        print("invalid response")
-    else:
-        print(f"to find {to_find}")
+   if solvable==True:
+      solved_param_values=solve(param_values)
+      solution=dict(zip(params,solved_param_values))
+      print(solution)
+   else:
+      print("invalid response")
 
-        print(f"we know {combined}")
-
-        print(f"solution is {solution}")
-
-params=["i_vel","f_vel","acc","dis","time"]
-param_values=input_params()
-#print(params)
-#print(param_values)
-not_solvable=solvable()
-to_find=to_find()
-i_vel,f_vel,acc,dis,time=param_values[0],param_values[1],param_values[2],param_values[3],param_values[4] #assign variables
-combined=dict(zip(params,param_values))
-solved_param_values=solve(param_values)
-solution=dict(zip(params,solved_param_values))
 main()
-
 
 
 
